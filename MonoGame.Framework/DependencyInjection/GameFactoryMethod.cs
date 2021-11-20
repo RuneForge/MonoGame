f464a4ab91fc770156e25d28f5f11fcfa762f4d3
Andrey Talanin <andrey.talanin@outlook.com>
2021-11-20 15:15:03 +0300

    Add the ServiceCollectionExtensions class containing extension methods for enabling dependency injection

---
+++ MonoGame.Framework/DependencyInjection/GameFactoryMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework.DependencyInjection
+{
+    /// <summary>
+    /// Represents a factory method creating an instance of the <see cref="Game" /> type.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider.</param>
+    /// <param name="gameWindow">The instance of a previously created game window.</param>
+    /// <returns>An instance of the <see cref="Game" /> type.</returns>
+    public delegate Game GameFactoryMethod(IServiceProvider serviceProvider, GameWindow gameWindow);
+}
