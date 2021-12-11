2778628480b3429c904597030a1f6a8968adcb54
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-11 13:04:52 +0300

    Add the MouseButtons flag enum

---
+++ MonoGame.Framework/Input/MouseButtons.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework.Input
+{
+    /// <summary>
+    /// Defines the buttons on a mouse.
+    /// </summary>
+    [Flags]
+    public enum MouseButtons
+    {
+        /// <summary>
+        /// Left mouse button.
+        /// </summary>
+        LeftButton = 1,
+
+        /// <summary>
+        /// Middle mouse button.
+        /// </summary>
+        MiddleButton = 2,
+
+        /// <summary>
+        /// Right mouse button.
+        /// </summary>
+        RightButton = 4,
+
+        /// <summary>
+        /// First extended button.
+        /// </summary>
+        XButton1 = 8,
+
+        /// <summary>
+        /// Second extended button.
+        /// </summary>
+        XButton2 = 16,
+    }
+}
