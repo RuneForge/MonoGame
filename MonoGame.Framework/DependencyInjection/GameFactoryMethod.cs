29a5422cbc543a77cd096f582f8f26fbe33909ce
Andrey Talanin <andrey.talanin@outlook.com>
2022-03-11 23:35:13 +0300

    Remove the GameWindow instance from the GameFactoryMethod parameters

--- MonoGame.Framework/DependencyInjection/GameFactoryMethod.cs
+++ MonoGame.Framework/DependencyInjection/GameFactoryMethod.cs
@@ -6,7 +6,6 @@ namespace Microsoft.Xna.Framework.DependencyInjection
     /// Represents a factory method creating an instance of the <see cref="Game" /> type.
     /// </summary>
     /// <param name="serviceProvider">The service provider.</param>
-    /// <param name="gameWindow">The instance of a previously created game window.</param>
     /// <returns>An instance of the <see cref="Game" /> type.</returns>
-    public delegate Game GameFactoryMethod(IServiceProvider serviceProvider, GameWindow gameWindow);
+    public delegate Game GameFactoryMethod(IServiceProvider serviceProvider);
 }
